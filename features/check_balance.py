6ff88cdf6f7b0e5716e7021331feb3df07730c2e
ao627515 <ao627515@gmail.com>
2025-06-21 09:44:48 +0000

    `Removed multiple files from the project: AppointmentAnalyzer.py, checkBalance.py, recharge.py, transfert.py, and GoogleCalendarManager.py.`

--- features/check_balance.py
+++
@@ -1,5 +0,0 @@
-def checkBalance(user_state):
-    return (
-        f"Votre solde est de {user_state['wallet_balance']} francs "
-        f"et votre crédit téléphonique est de {user_state['credit_balance']} francs."
-    )
